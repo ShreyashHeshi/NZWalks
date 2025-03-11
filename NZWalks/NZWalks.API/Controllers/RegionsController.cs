80819b21af1e3d25407b0d3fbe97a6ea41fce7cd
thehesishreyash <shreyashesi3@gmail.com>
2025-03-11 19:10:28 +0530

    database integration added

---
+++ NZWalks/NZWalks.API/Controllers/RegionsController.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using NZWalks.API.Data;
+
+namespace NZWalks.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class RegionsController : ControllerBase
+    {
+        private readonly NZWalksDbContext dbContext;
+        public RegionsController(NZWalksDbContext dbContext)
+        {
+            this.dbContext = dbContext;
+        }
+
+        // get all regions
+        // https://localhost:7084/api/Regions
+        [HttpGet]
+        public IActionResult GetAll()
+        {
+           var regions = dbContext.Regions.ToList();
+            return Ok(regions);
+        }
+
+        // Get region by id
+
+        [HttpGet]
+        [Route("{id:Guid}")]
+        public IActionResult GetById([FromRoute] Guid id)
+        {
+            //var region = dbContext.Regions.Find(id);
+
+            // second way to find id by Linq method FirstOrDefault
+            var region = dbContext.Regions.FirstOrDefault(x => x.Id == id);
+            if(region == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(region);
+
+
+        }
+
+
+    }
+}
