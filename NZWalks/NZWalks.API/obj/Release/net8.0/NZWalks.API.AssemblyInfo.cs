5f1486bc304d4dcf8742d99a45782fbcf1ce1fb9
thehesishreyash <shreyashesi3@gmail.com>
2025-03-18 19:13:16 +0530

    dp added

--- NZWalks/NZWalks.API/obj/Release/net8.0/NZWalks.API.AssemblyInfo.cs
+++ NZWalks/NZWalks.API/obj/Release/net8.0/NZWalks.API.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("NZWalks.API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3aa551c6a24b7efafa3e315ef8cf8c41da564957")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+006016bdc0a013076c9e0b9a4705e21ee36b547f")]
 [assembly: System.Reflection.AssemblyProductAttribute("NZWalks.API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("NZWalks.API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
